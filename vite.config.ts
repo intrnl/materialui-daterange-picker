29fb1fc1c5e61dc366b5dda8d993fcec977211db
intrnl <intrnl28+git@gmail.com>
2022-12-22 07:08:49 +0000

    chore: attempt to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
+import * as path from 'node:path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
+  build: {
+    lib: {
+      formats: ['es', 'cjs'],
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      fileName: 'materialui-daterange-picker',
+    },
+    rollupOptions: {
+      external: [
+        '@material-ui/core',
+        '@material-ui/icons',
+        'date-fns',
+        'react',
+        'react/jsx-runtime',
+        'react-dom',
+      ],
+      output: {
+        freeze: false,
+      },
+    },
+  },
   plugins: [
     react(),
   ],
