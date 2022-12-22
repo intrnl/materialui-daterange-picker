2a1b20835c4fb7935a3ec43e31e70df422b7e85c
intrnl <intrnl28+git@gmail.com>
2022-12-22 05:25:24 +0000

    chore: remove nonexistant definedrange

--- package/lib/index.ts
+++ package/lib/index.ts
@@ -1,8 +1,7 @@
 import DateRangePicker from './components/DateRangePicker';
-import { type DateRange, type DefinedRange } from './types';
+import { type DateRange } from './types';
 
 export {
   DateRangePicker,
-  type DefinedRange,
   type DateRange,
 };
